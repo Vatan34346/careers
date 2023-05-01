08aae8d35c8f872c4a3895319b05536b5ac92856
Vatan34346 <paatagvichia@gmail.com>
2023-05-01 20:51:42 +0400

    jobs -> JOBS list

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', jobs=jobs, company_name='Paata')
+    return render_template('home.html', jobs=JOBS, company_name='Paata')
 
 
 @app.route('/jobs')
