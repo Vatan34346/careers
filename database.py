703f59bf7076803fa9a3a2f38e6acb7dba717536
Vatan34346 <paatagvichia@gmail.com>
2023-05-11 20:05:12 +0400

    add dynamic pages

--- database.py
+++ database.py
@@ -4,7 +4,6 @@ from sqlalchemy import create_engine, text
 
 load_dotenv()
 
-print(os.getenv('CONNECTION_STRING'))
 engine = create_engine(os.getenv('CONNECTION_STRING'),
                        connect_args={
                            "ssl": {
@@ -20,9 +19,19 @@ def load_jobs():
         result = conn.execute(text("select * from jobs"))
         columns = result.keys()
         jobs_ = [dict(zip(columns, row)) for row in result.all()]
-        print(jobs_)
         return jobs_
 
 
+def load_job_from_db(id_):
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM jobs WHERE id = :id"), {"id": id_})
+        rows = result.all()
+        columns = result.keys()
+        print(columns)
+        if len(rows) == 0:
+            return None
+        else:
+            job = [dict(zip(columns, row)) for row in rows][0]
+            return job
 
 
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs
+from database import load_jobs, load_job_from_db
 
 app = Flask(__name__)
 
@@ -16,6 +16,14 @@ def home():
 def jobs():
     jobs_ = load_jobs()
     return jsonify(jobs_)
+
+
+@app.route('/job/<id_>')
+def show_job(id_):
+    job = load_job_from_db(id_)
+    if not job:
+        return 'Not Found',404
+    return render_template('jobpage.html', job=job)
 
 
 if __name__ == '__main__':
